60ed145bf7be08cf3af14628ffba56a2007aa206
yjw <yejw6@asiainfo.com>
2022-03-03 17:28:59 +0800

    doc: 插入排序

---
+++ leetCode/插入排序.js
@@ -0,0 +1,10 @@
+Array.prototype.insertionSort = function() {
+  const tmp = this[1]
+  
+}
+
+const arr = [1, 4, 2, 6, 3, 7, 5, 8, 9];
+console.log("---------插入排序----------");
+console.log(arr);
+arr.insertionSort();
+console.log(arr);
