c227f0e1cd5b81b7fcd7e67df29627de036a4567
yjw <yejw6@asiainfo.com>
2022-03-04 16:43:51 +0800

    feat: 插入排序

--- leetCode/插入排序.js
+++ leetCode/插入排序.js
@@ -1,9 +1,24 @@
-Array.prototype.insertionSort = function() {
-  const tmp = this[1]
-  
-}
+/* 
+  时间复杂度：O(n2)
+*/
+Array.prototype.insertionSort = function () {
+  for (let i = 1; i < this.length; i++) {
+    const tmp = this[i];
+    let j = i;
+    while (j > 0) {
+      if (this[j - 1] > tmp) {
+        // 往后排，先不插入
+        this[j] = this[j - 1];
+      } else {
+        break;
+      }
+      j--;
+    }
+    this[j] = tmp;
+  }
+};
 
-const arr = [1, 4, 2, 6, 3, 7, 5, 8, 9];
+const arr = [4, 1, 2, 6, 3, 7, 5, 8, 9];
 console.log("---------插入排序----------");
 console.log(arr);
 arr.insertionSort();
---
+++ 高级前端开发工程师/一、计算机网络/11_Promise和fetch.md
@@ -0,0 +1,8 @@
+# 1. Promise 神器
+pending
+fulfilled
+rejected
+
+# 2. fetch 的基本用法
+
+# 3. fetch 和 Promise 场景举例
