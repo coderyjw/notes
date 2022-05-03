08887169ec0017ae0a2ba792c68dd3732b832ea5
yjw <yejw6@asiainfo.com>
2022-05-03 16:59:38 +0800

    webpack+react+ts项目配置

---
+++ 高级前端开发工程师/07_ts/react-proj/build/main.js
@@ -0,0 +1,9 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+exports.__esModule = true;
+var react_dom_1 = __importDefault(require("react-dom"));
+var react_1 = __importDefault(require("react"));
+var App_1 = __importDefault(require("./App"));
+react_dom_1["default"].render(react_1["default"].createElement(App_1["default"], null), document.getElementById('root'));
