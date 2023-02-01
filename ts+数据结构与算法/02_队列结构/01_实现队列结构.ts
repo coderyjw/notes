5ad04416a9fe2243992f283e036fc9f35ee1162e
叶继伟 <yejw6@asiainfo.com>
2023-02-01 17:31:40 +0800

    feat: 实现队列结构

---
+++ ts+数据结构与算法/02_队列结构/01_实现队列结构.ts
@@ -0,0 +1,32 @@
+// 封装一个队列
+export default class ArrayQueue<T = any> {
+  private data: T[] = [];
+
+  push(element: T): void {
+    this.data.push(element);
+  }
+
+  pop(): T | undefined {
+    return this.data.shift();
+  }
+
+  peek(): T | undefined {
+    return this.data[0];
+  }
+
+  isEmpty(): boolean {
+    return this.data.length === 0;
+  }
+
+  size(): number {
+    return this.data.length;
+  }
+}
+
+const queue = new ArrayQueue<number>();
+
+queue.push(1);
+queue.push(2);
+queue.pop();
+queue.push(3);
+console.log(queue); // ArrayQueue { data: [ 2, 3 ] }
