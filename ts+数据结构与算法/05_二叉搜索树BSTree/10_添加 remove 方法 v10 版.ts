6eba66a63e19dd289108b6bf78b34ccbdc764cee
叶继伟 <yejw6@asiainfo.com>
2023-02-08 01:17:05 +0800

    feat: 10_添加 remove 方法 v10 版

--- ts+数据结构与算法/05_二叉搜索树BSTree/10_添加 remove 方法 v10 版.ts
+++ ts+数据结构与算法/05_二叉搜索树BSTree/10_添加 remove 方法 v10 版.ts
@@ -139,24 +139,128 @@ class BSTree<T> {
         current = current.left;
       }
     }
-    
 
     return false;
+  }
+
+  remove(value: T): boolean {
+    // 1. 获取要删除的节点 current 和 其父节点 parent
+    let current = this.root;
+    let parent: TreeNode<T> | null = null;
+    while (current) {
+      // 找到了节点
+      if (current.value === value) break;
+
+      parent = current;
+
+      if (current.value < value) {
+        current = current.right;
+      } else {
+        current = current.left;
+      }
+    }
+    // 如果没有找到要删除的节点返回 false
+    if (!current) return false;
+
+    // 2.  如果要删除的的节点是叶子结点
+    if (current.left === null && current.right === null) {
+      if (current === this.root) {
+        // current 是根节点
+        this.root = null;
+      } else if (parent?.left === current) {
+        // current 在父节点的左边
+        parent!.left = null;
+      } else {
+        // current 在父节点的右边
+        parent!.right = null;
+      }
+    }
+
+    // 3. 如果要删除的的节点只有一个左子节点
+    else if (current.right === null) {
+      if (current === this.root) {
+        this.root = current.left;
+      } else if (parent?.left === current) {
+        // current 在父节点的左边
+        parent!.left = current.left;
+      } else {
+        parent!.right = current.left;
+      }
+    }
+
+    // 4. 如果要删除的的节点只有一个右子节点
+    else if (current.left === null) {
+      if (current === this.root) {
+        this.root = current.right;
+      } else if (parent?.left === current) {
+        // current 在父节点的左边
+        parent!.left = current.right;
+      } else {
+        parent!.right = current.right;
+      }
+    }
+
+    // 5. 如果要删除的的节点有两个子节点
+    else {
+      const successor = this.getSuccessor(current);
+      if (current === this.root) {
+        this.root = successor;
+      } else if (parent?.left === current) {
+        parent!.left = successor;
+      } else {
+        parent!.right = successor;
+      }
+    }
+    return true;
+  }
+
+  // 获取后继节点
+  private getSuccessor(delNode: TreeNode<T>): TreeNode<T> {
+    // 获取右子树
+    let current = delNode.right;
+    let successor: TreeNode<T> | null = null;
+    while (current) {
+      successor = current;
+      current = current.left;
+    }
+
+    // 拿到了后继节点
+    if (successor !== delNode.right) {
+      delNode!.right!.left = successor?.right ?? null;
+      successor!.right = delNode.right;
+    }
+
+    // 一定要进行的操作：将删除几点的 left 赋值给后继节点的 left
+    successor!.left = delNode.left;
+
+    return successor!;
   }
 }
 
 const bst = new BSTree<number>();
 
+bst.insert(11);
+bst.insert(7);
+bst.insert(15);
+bst.insert(5);
+bst.insert(9);
+bst.insert(13);
 bst.insert(20);
-bst.insert(30);
-bst.insert(18);
-bst.insert(14);
+bst.insert(3);
+bst.insert(8);
+bst.insert(10);
 bst.insert(12);
+bst.insert(14);
+bst.insert(18);
+bst.insert(25);
 bst.insert(19);
-bst.insert(22);
 
-console.log(bst.search(12));
-console.log(bst.search(1));
+btPrint(bst.root);
+bst.remove(14);
+
+btPrint(bst.root);
+// bst.remove(14);
+// btPrint(bst.root);
 // console.log(bst.getMaxValue());
 // console.log(bst.getMinValue());
 // bst.levelOrderTraverse();
