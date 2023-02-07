5b6b355bd5cc9916948df6099357840a53103abd
叶继伟 <yejw6@asiainfo.com>
2023-02-07 22:02:38 +0800

    feat: 封装 二叉搜索树 v1

---
+++ ts+数据结构与算法/05_二叉搜索树BSTree/01_二叉搜索树BSTree.ts
@@ -0,0 +1,13 @@
+class TreeNode<T> {
+  value: T;
+  left: TreeNode<T> | null = null;
+  right: TreeNode<T> | null = null;
+
+  constructor(value: T) {
+    this.value = value;
+  }
+}
+
+class BSTree<T> {
+  root: TreeNode<T> | null = null;
+}
