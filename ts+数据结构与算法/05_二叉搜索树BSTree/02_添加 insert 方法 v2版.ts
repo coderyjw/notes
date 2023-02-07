31f3bc078fb40bab85294b1ef6d0e83fc5a74cd7
叶继伟 <yejw6@asiainfo.com>
2023-02-07 22:03:11 +0800

    feat: BSTree 添加 insert 方法 v2 版本

---
+++ ts+数据结构与算法/05_二叉搜索树BSTree/02_添加 insert 方法 v2版.ts
@@ -0,0 +1,68 @@
+import { btPrint } from "hy-algokit";
+
+class TreeNode<T> {
+  value: T;
+  left: TreeNode<T> | null = null;
+  right: TreeNode<T> | null = null;
+
+  constructor(value: T) {
+    this.value = value;
+  }
+}
+
+class BSTree<T> {
+  root: TreeNode<T> | null = null;
+
+  insert(value: T) {
+    // 1. 根据传入 value 创建 TreeNode 节点
+    const newNode = new TreeNode(value);
+
+    // 2. 判断当前是否已经有了根节点
+    if (!this.root) {
+      // 当前树为空
+      this.root = newNode;
+    } else {
+      // 树中已经有其他值
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  private insertNode(node: TreeNode<T>, newNode: TreeNode<T>) {
+    if (newNode.value < node.value) {
+      // 去左边查找空白位置
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      // 去右边查找空白位置
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+}
+
+const bst = new BSTree<number>();
+
+bst.insert(20);
+bst.insert(30);
+bst.insert(18);
+bst.insert(14);
+bst.insert(12);
+bst.insert(19);
+bst.insert(22);
+
+btPrint(bst.root);
+//                20
+//         ┌───────┴───────┐
+//        18              30
+//     ┌───┴───┐       ┌───┘
+//    14      19      22
+//   ┌─┘
+//  12
+
+export {};
