3880e6843a098558548627b7383d23bc756606c7
叶继伟 <yejw6@asiainfo.com>
2023-02-08 22:04:20 +0800

    feat: 03_深度优先遍历 v2 版

--- ts+数据结构与算法/06_图/03_深度优先遍历 v2 版.ts
+++ ts+数据结构与算法/06_图/03_深度优先遍历 v2 版.ts
@@ -18,13 +18,42 @@ class Graph<T> {
     this.adjList.get(v2)?.push(v1);
   }
 
-traverse() {
-  this.verteces.forEach((vertex) => {
-    const edges = this.adjList.get(vertex);
+  traverse() {
+    this.verteces.forEach((vertex) => {
+      const edges = this.adjList.get(vertex);
 
-    console.log(`${vertex} -> ${edges?.join(" ")}`);
-  });
-}
+      console.log(`${vertex} -> ${edges?.join(" ")}`);
+    });
+  }
+
+  dfs() {
+    // 1. 判断是否有顶点
+    if (this.verteces.length === 0) return;
+
+    // 2. 创建队列结构访问每一个顶点
+    const stack: T[] = [];
+    stack.push(this.verteces[0]);
+
+    // 3. 创建 Set 结构，记录某一个顶点是否被访问过
+    const visited = new Set<T>();
+    visited.add(this.verteces[0]);
+
+    // 4. 从第一个顶点开始访问
+    while (stack.length) {
+      const vertex = stack.pop()!;
+      console.log(vertex);
+
+      const neighbors = this.adjList.get(vertex);
+      if (!neighbors) continue;
+      for (let i = neighbors.length - 1; i >= 0; i--) {
+        const nei = neighbors[i];
+        if (!visited.has(nei)) {
+          visited.add(nei);
+          stack.push(nei);
+        }
+      }
+    }
+  }
 }
 
 const graph = new Graph();
@@ -44,4 +73,5 @@ graph.addEdge("C", "D");
 graph.addEdge("F", "A");
 
 graph.traverse();
+graph.dfs();
 export {};
